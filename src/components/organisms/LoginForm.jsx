e074c0e09b0c3ae7950fe5cfef1c3227a4756366
Numonu <juan.villegas.dev@gmail.com>
2023-05-13 13:38:30 -0500

    he maquetado la estructura del componente LoginForm

--- src/components/organisms/LoginForm.jsx
+++ src/components/organisms/LoginForm.jsx
@@ -1,7 +1,24 @@
-export default function LoginForm(){
-    return <>
-        <div>
-            
-        </div>
-    </>
+import Input from "../atoms/Input";
+import { AiOutlineUser } from "react-icons/ai";
+import { BsShieldLock } from "react-icons/bs";
+import Button from "../atoms/Button";
+
+export default function LoginForm() {
+	return (
+		<>
+			<div>
+				<Input
+					placeholder="email address"
+					icon={<AiOutlineUser />}
+					required
+				/>
+				<Input
+					placeholder="password"
+					icon={<BsShieldLock />}
+					required
+				/>
+                <Button></Button>
+			</div>
+		</>
+	);
 }
