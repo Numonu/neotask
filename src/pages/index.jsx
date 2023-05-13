c89b99e65ecf22e02048c044e1ca1baebb64ac40
Numonu <juan.villegas.dev@gmail.com>
2023-05-12 20:57:58 -0500

    primer commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export  default function Home(){
+    return <>
+        <Head>
+            <title>Neotask|Numonu</title>
+        </Head>
+        <div>
+            <h1 className="text-2xl text-emerald-500">Hello World</h1>
+        </div>
+    </>
+}
