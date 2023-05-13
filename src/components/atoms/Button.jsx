815b360efe13f151826c94097257f7ce441848bd
Numonu <juan.villegas.dev@gmail.com>
2023-05-13 13:40:04 -0500

    he agregado los props de configuracion para el componente button

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,7 +1,7 @@
-export default function Button({className}){
+export default function Button({className , children="text here"}){
     return <>
         <button className={`bg-neutral-800 text-neutral-50 border-black w-full py-2 text-xl font-medium rounded-md border-b-4 hover:bg-amber-500 hover:border-amber-700 transition-colors ${className}`}>
-            Tamare oh
+            {children}
         </button>
     </>
 }
