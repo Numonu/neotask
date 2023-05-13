2f9b9421a4b5b1436cbd6c55fe60759dc0890466
Numonu <juan.villegas.dev@gmail.com>
2023-05-12 22:02:01 -0500

    he maquetado un poco la pagina login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,13 +1,28 @@
 import Input from "@/components/atoms/Input";
+import Title from "@/components/atoms/Title";
+import { AiOutlineUser , AiOutlineMail } from "react-icons/ai";
 import Head from "next/head";
 
-export default function Login(){
-    return <>
-        <Head>
-            <title>Login in Neotask</title>
-        </Head>
-        <div className="text-neutral-900 bg-neutral-50 min-h-screen">
-            <Input/>
-        </div>
-    </>
+export default function Login() {
+	return (
+		<>
+			<Head>
+				<title>Login in Neotask</title>
+			</Head>
+			<div className="text-neutral-900 bg-neutral-50 min-h-screen">
+				<Title>Login</Title>
+				<Input
+					placeholder="Email Address"
+					icon={<AiOutlineUser />}
+					required
+				/>
+                <Input
+					placeholder="Password"
+                    type="password"
+					icon={<AiOutlineMail />}
+					required
+				/>
+			</div>
+		</>
+	);
 }
