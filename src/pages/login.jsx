fbb34e53bf6421ccba02232e7bf7a4bcf0d9026d
Numonu <juan.villegas.dev@gmail.com>
2023-05-13 15:24:43 -0500

    he importado LoginLAyout a la pagina Login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,5 @@
-import Title from "@/components/atoms/Title";
 import Head from "next/head";
-import LoginForm from "@/components/molecules/LoginForm";
-import MyLink from "@/components/atoms/Link";
+import LoginLayout from "@/components/organisms/LoginLayout";
 
 export default function Login() {
 	return (
@@ -11,15 +9,7 @@ export default function Login() {
 			</Head>
 			<div className="text-neutral-900 bg-neutral-50">
 				<div className="min-h-screen max-w-xl mx-auto flex flex-col justify-center gap-4">
-					<Title>Login</Title>
-					<p className="text-xl">
-						Bienvenido! , inicia sesion y accede a todas tus tareas
-						desde cualquier parte del mundo
-					</p>
-					<LoginForm />
-					<span className="text-center">
-						No tienes una cuenta?<MyLink>Crear aqui!</MyLink>
-					</span>
+					<LoginLayout/>
 				</div>
 			</div>
 		</>
