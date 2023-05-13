2b6b874b368ab425738802bce630e088993692ee
Numonu <juan.villegas.dev@gmail.com>
2023-05-12 21:35:56 -0500

    he creado el diseño del componente Input

--- src/components/atoms/Input.jsx
+++ src/components/atoms/Input.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+import { AiOutlineUser } from "react-icons/ai";
 export default function Input() {
+	const [focus, setFocus] = useState(false);
+
+	const on = () => setFocus(true);
+	const off = () => setFocus(false);
+
 	return (
 		<>
-			<div></div>
+			<div
+				className={`${focus && "border-amber-500"} 
+                border-black border-2 rounded-lg p-2 flex items-center gap-2 transition-colors`}
+				onFocus={on}
+				onBlur={off}
+			>
+				<div
+					className={`${focus && "text-amber-500"} 
+                    text-3xl transition-colors`}
+				>
+					<AiOutlineUser />
+				</div>
+				<input
+					className="text-xl grow outline-none"
+					type="text"
+					placeholder="Full Name"
+					required
+				/>
+			</div>
 		</>
 	);
 }
