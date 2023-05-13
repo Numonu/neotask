22aa6c94ff1a16f1a9a9f1a5388e5b711d36300b
Numonu <juan.villegas.dev@gmail.com>
2023-05-13 13:21:31 -0500

    he agregado transicion de colores en el componente Button

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,6 +1,6 @@
 export default function Button(){
     return <>
-        <button className="bg-neutral-800 text-neutral-50 border-black w-full py-2 text-xl font-medium rounded-md border-b-4 hover:bg-amber-500 hover:border-amber-700">
+        <button className="bg-neutral-800 text-neutral-50 border-black w-full py-2 text-xl font-medium rounded-md border-b-4 hover:bg-amber-500 hover:border-amber-700 transition-colors">
             Tamare oh
         </button>
     </>
