96dfef5ab941f7b562a06018b69c07aeb8dbcb64
Numonu <juan.villegas.dev@gmail.com>
2023-05-13 14:23:15 -0500

    he estilizado el layout de la pagina login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,7 @@
 import Title from "@/components/atoms/Title";
 import Head from "next/head";
-import LoginForm from "@/components/organisms/LoginForm";
+import LoginForm from "@/components/molecules/LoginForm";
+import MyLink from "@/components/atoms/Link";
 
 export default function Login() {
 	return (
@@ -8,12 +9,18 @@ export default function Login() {
 			<Head>
 				<title>Login in Neotask</title>
 			</Head>
-			<div className="text-neutral-900 bg-neutral-50 min-h-screen">
-				<Title>Login</Title>
-				<p className="text-xl">
-					Bienvenido! , inicia sesion y accede a todas tus tareas desde cualquier parte del mundo
-				</p>
-				<LoginForm/>
+			<div className="text-neutral-900 bg-neutral-50">
+				<div className="min-h-screen max-w-xl flex flex-col justify-center gap-4">
+					<Title>Login</Title>
+					<p className="text-xl">
+						Bienvenido! , inicia sesion y accede a todas tus tareas
+						desde cualquier parte del mundo
+					</p>
+					<LoginForm />
+					<span className="text-center">
+						No tienes una cuenta ? <MyLink>Crear aqui!</MyLink>
+					</span>
+				</div>
 			</div>
 		</>
 	);
