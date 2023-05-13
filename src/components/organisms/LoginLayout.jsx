75697ddc5edfcece67d174814145eef81bde7055
Numonu <juan.villegas.dev@gmail.com>
2023-05-13 15:23:19 -0500

    he maquetado el contenido del componente LoginLayout

--- src/components/organisms/LoginLayout.jsx
+++ src/components/organisms/LoginLayout.jsx
@@ -1,7 +1,21 @@
-export default function LoginLayout(){
-    return <>
-        <div>
-            
-        </div>
-    </>
+import MyLink from "../atoms/Link";
+import Title from "../atoms/Title";
+import LoginForm from "../molecules/LoginForm";
+
+export default function LoginLayout() {
+	return (
+		<>
+			<div>
+				<Title>Login</Title>
+				<p className="text-xl">
+					Bienvenido! , inicia sesion y accede a todas tus tareas
+					desde cualquier parte del mundo
+				</p>
+				<LoginForm />
+				<span className="text-center">
+					No tienes una cuenta?<MyLink>Crear aqui!</MyLink>
+				</span>
+			</div>
+		</>
+	);
 }
