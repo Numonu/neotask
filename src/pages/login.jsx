cd6851a9d98c7a913286fd566bf8354dcfef1db9
Numonu <juan.villegas.dev@gmail.com>
2023-05-13 15:25:44 -0500

    he eliminado el gap en la pagina login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -8,7 +8,7 @@ export default function Login() {
 				<title>Login in Neotask</title>
 			</Head>
 			<div className="text-neutral-900 bg-neutral-50">
-				<div className="min-h-screen max-w-xl mx-auto flex flex-col justify-center gap-4">
+				<div className="min-h-screen max-w-xl mx-auto flex flex-col justify-center">
 					<LoginLayout/>
 				</div>
 			</div>
