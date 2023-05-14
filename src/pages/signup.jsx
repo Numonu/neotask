852f7d7d04688539aadcd263d9f7af6ddfb131a7
Numonu <juan.villegas.dev@gmail.com>
2023-05-13 22:25:10 -0500

    he maquetado el contenido de la pagina signup

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,3 +1,4 @@
+import SignUpLayout from "@/components/organisms/SignUpLayout";
 import Head from "next/head";
 
 export default function SignUp() {
@@ -8,6 +9,7 @@ export default function SignUp() {
 			</Head>
 			<div className="text-neutral-900 bg-neutral-50">
 				<div className="min-h-screen max-w-xl mx-auto flex flex-col justify-center">
+                    <SignUpLayout/>
 				</div>
 			</div>
 		</>
