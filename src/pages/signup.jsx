61300465d13bc41872b5077527c1c19419b60dda
Numonu <juan.villegas.dev@gmail.com>
2023-05-14 14:09:57 -0500

    he reducido el peso del color de texto en signup y login

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -7,7 +7,7 @@ export default function SignUp() {
 			<Head>
 				<title>Sign Up in Neotask</title>
 			</Head>
-			<div className="text-neutral-900 bg-neutral-50">
+			<div className="text-neutral-800 bg-neutral-50">
 				<div className="min-h-screen max-w-xl mx-auto flex flex-col justify-center">
                     <SignUpLayout/>
 				</div>
