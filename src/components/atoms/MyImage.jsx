60d0f9bb760dae46c04c9f734d689ad1cffcdb6d
Numonu <juan.villegas.dev@gmail.com>
2023-05-14 17:01:10 -0500

    he modificado la myImage para aceptar imagenes de relleno o tamaño definido

--- src/components/atoms/MyImage.jsx
+++ src/components/atoms/MyImage.jsx
@@ -1,10 +1,30 @@
 import Image from "next/image";
 
-export default function MyImage({ className, src, alt }) {
+export default function MyImage({ className, src, alt, fill, width, height }) {
+	if (fill) {
+		return (
+			<>
+				<div className={`${className} relative`}>
+					<Image
+						src={src}
+						alt={alt}
+						layout="fill"
+						objectFit="cover"
+					/>
+				</div>
+			</>
+		);
+	}
 	return (
 		<>
 			<div className={`${className} relative`}>
-				<Image src={src} alt={alt} layout="fill" objectFit="cover" />
+				<Image
+					src={src}
+					alt={alt}
+					width={width}
+					height={height}
+					objectFit="cover"
+				/>
 			</div>
 		</>
 	);
