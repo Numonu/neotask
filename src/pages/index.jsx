c998179a501d060cc0a7b4c5bf0bb9a561789b90
Numonu <juan.villegas.dev@gmail.com>
2023-05-14 13:58:50 -0500

    he importado el componente homeHero a la pagina index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,20 @@
+import { BsFillRocketTakeoffFill } from "react-icons/bs";
+import { GiParrotHead } from "react-icons/gi";
+import Button from "@/components/atoms/Button";
+import Paragraph from "@/components/atoms/Paragraph";
+import Subtitle from "@/components/atoms/Subtitle";
 import Head from "next/head";
+import HomeHero from "@/components/organisms/HomeHero";
 
-export  default function Home(){
-    return <>
-        <Head>
-            <title>Neotask|Numonu</title>
-        </Head>
-        <div>
-            <h1 className="text-2xl text-emerald-500">Hello World</h1>
-        </div>
-    </>
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>Welcome to Neotask</title>
+			</Head>
+			<div className="bg-neutral-50 text-neutral-900 min-h-screen">
+				<HomeHero />
+			</div>
+		</>
+	);
 }
