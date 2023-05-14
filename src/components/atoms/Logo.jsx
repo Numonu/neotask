ae5a7441358471cf73c5ee3a26fc4a0488a70762
Numonu <juan.villegas.dev@gmail.com>
2023-05-14 14:08:54 -0500

    he reducido el peso del color en el borde del componente Logo

--- src/components/atoms/Logo.jsx
+++ src/components/atoms/Logo.jsx
@@ -3,7 +3,7 @@ import { GiParrotHead } from "react-icons/gi";
 export default function Logo() {
 	return (
 		<>
-			<div className="bg-neutral-50 w-min mx-auto rounded-full p-4 text-6xl border-4 border-neutral-900">
+			<div className="bg-neutral-50 w-min mx-auto rounded-full p-4 text-6xl border-4 border-neutral-800">
 				<GiParrotHead />
 			</div>
 		</>
