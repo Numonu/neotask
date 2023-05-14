264c4dd5de7d3bd7778a91cf9c771181293b7b70
Numonu <juan.villegas.dev@gmail.com>
2023-05-14 13:29:55 -0500

    he creado y maquetado el componente logo

---
+++ src/components/atoms/Logo.jsx
@@ -0,0 +1,11 @@
+import { GiParrotHead } from "react-icons/gi";
+
+export default function Logo() {
+	return (
+		<>
+			<div className="bg-neutral-50 rounded-full p-4 text-6xl border-4 border-neutral-900">
+				<GiParrotHead />
+			</div>
+		</>
+	);
+}
