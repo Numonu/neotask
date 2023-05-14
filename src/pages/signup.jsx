17251560bd26e8ff91170386405ee7d43b638f07
Numonu <juan.villegas.dev@gmail.com>
2023-05-13 22:00:24 -0500

    he creado la pagina signup

---
+++ src/pages/signup.jsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function SignUp() {
+	return (
+		<>
+			<Head>
+				<title>Sign Up in Neotask</title>
+			</Head>
+			<div className="text-neutral-900 bg-neutral-50">
+				<div className="min-h-screen max-w-xl mx-auto flex flex-col justify-center">
+				</div>
+			</div>
+		</>
+	);
+}
