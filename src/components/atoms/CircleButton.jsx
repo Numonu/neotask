ee8cbad2dc950f053b36c88337da7816dd6bb023
Numonu <juan.villegas.dev@gmail.com>
2023-05-15 13:16:34 -0500

    he ajustado el color del componente circleButton

--- src/components/atoms/CircleButton.jsx
+++ src/components/atoms/CircleButton.jsx
@@ -2,7 +2,7 @@ export default function CircleButton({children}) {
 	return (
 		<>
 			<button className="group relative p-2 cursor-pointer">
-				<div className="bg-neutral-300 absolute inset-0 rounded-full scale-0 group-hover:scale-100 transition-transform"></div>
+				<div className="bg-neutral-200 absolute inset-0 rounded-full scale-0 group-hover:scale-100 transition-transform"></div>
 				<div className="relative">
                     {children}
                 </div>
