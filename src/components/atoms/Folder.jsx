af7514f9897cfb7ad68f42d3f46b71a117df1e12
Numonu <juan.villegas.dev@gmail.com>
2023-05-15 16:51:56 -0500

    he agregado un efecto de focus en el componente folder

--- src/components/atoms/Folder.jsx
+++ src/components/atoms/Folder.jsx
@@ -1,9 +1,9 @@
 export default function FolderButton({ icon, children }) {
 	return (
 		<>
-			<button className="bg-neutral-300 py-2 px-4 flex items-center gap-2 rounded-lg hover:bg-neutral-800 hover:text-neutral-50 hover:py-3 transition-[background-color_padding_color]">
-				<div className="text-2xl">{icon}</div>
-				<span className="text-xl">{children}</span>
+			<button className="bg-neutral-300 py-2 px-4 flex items-center gap-2 rounded-lg hover:bg-neutral-800 hover:text-neutral-50 hover:py-3 transition-[background-color_padding_color] focus:bg-neutral-800  focus:py-3 focus:outline-amber-500 group">
+				<div className="text-2xl group-focus:text-amber-500">{icon}</div>
+				<span className="text-xl group-focus:text-amber-500">{children}</span>
 			</button>
 		</>
 	);
