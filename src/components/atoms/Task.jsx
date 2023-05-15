d84628a918d0b9d14f1fcd11e697cec2b99192ac
Numonu <juan.villegas.dev@gmail.com>
2023-05-15 17:05:24 -0500

    he agregado el fondo de  patrones en el componente task

--- src/components/atoms/Task.jsx
+++ src/components/atoms/Task.jsx
@@ -1,13 +1,13 @@
 export default function Task({title , date}) {
 	return (
 		<>
-			<div className="bg-red-500 text-neutral-50 border-neutral-800 border-2 border-b-4 px-4 py-6 rounded-md">
-				<div className="border-neutral-50 w-9 aspect-square border-4 rounded-xl mb-8"></div>
+			<button className="pattern-points border-neutral-800 border-2 border-b-4 px-4 py-6 rounded-md text-start">
+				<div className="border-neutral-800 w-9 aspect-square border-4 rounded-xl mb-8"></div>
 				<h2 className="font-bold text-3xl mb-2">
 					{title}
 				</h2>
 				<h3 className="font-medium">{date}</h3>
-			</div>
+			</button>
 		</>
 	);
 }
