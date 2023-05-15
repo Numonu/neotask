0918761b7a38c9900742fdf2a1fec5e870b48782
Numonu <juan.villegas.dev@gmail.com>
2023-05-15 16:53:38 -0500

    he agregado un efecto de focus en el componente circleButton

--- src/components/atoms/CircleButton.jsx
+++ src/components/atoms/CircleButton.jsx
@@ -1,8 +1,8 @@
 export default function CircleButton({children}) {
 	return (
 		<>
-			<button className="group relative p-2 cursor-pointer">
-				<div className="bg-neutral-200 absolute inset-0 rounded-full scale-0 group-hover:scale-100 transition-transform"></div>
+			<button className="group relative p-2 cursor-pointer rounded-full outline-amber-500">
+				<div className="bg-neutral-200 absolute inset-0 rounded-full scale-0 group-hover:scale-100 group-focus:scale-100 transition-transform"></div>
 				<div className="relative">
                     {children}
                 </div>
