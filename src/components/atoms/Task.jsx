55a25b23b5337af67ac4774683efeda8b4a87ac6
Numonu <juan.villegas.dev@gmail.com>
2023-05-15 17:26:12 -0500

    he agregado un sutil efecto de hover en el componente task

--- src/components/atoms/Task.jsx
+++ src/components/atoms/Task.jsx
@@ -1,8 +1,8 @@
 export default function Task({title , date}) {
 	return (
 		<>
-			<button className="pattern-points border-neutral-800 border-2 border-b-4 px-4 py-6 rounded-md text-start outline-offset-8 outline-amber-500">
-				<div className="border-neutral-800 w-9 aspect-square border-4 rounded-xl mb-8"></div>
+			<button className="pattern-points border-neutral-800 border-2 border-b-4 px-4 py-6 rounded-md text-start outline-offset-8 outline-amber-500 group">
+				<div className="border-neutral-800 w-9 aspect-square border-4 rounded-xl mb-8 group-hover:rotate-45 transition-transform"></div>
 				<h2 className="font-bold text-3xl mb-2">
 					{title}
 				</h2>
