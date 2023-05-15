39a30f4597a1a5a0028122bfe926c40d5218987c
Numonu <juan.villegas.dev@gmail.com>
2023-05-14 20:47:23 -0500

    he quitado metodos redundantes del componente Input

--- src/components/atoms/Input.jsx
+++ src/components/atoms/Input.jsx
@@ -2,16 +2,13 @@ import { useState } from "react";
 export default function Input({icon , placeholder , required , type="text"}) {
 	const [focus, setFocus] = useState(false);
 
-	const on = () => setFocus(true);
-	const off = () => setFocus(false);
-
 	return (
 		<>
 			<div
 				className={`${focus ? "border-amber-500" : "border-black"} 
                 border-2 rounded-lg p-2 flex items-center gap-2 transition-colors`}
-				onFocus={on}
-				onBlur={off}
+				onFocus={() => setFocus(true)}
+				onBlur={() => setFocus(false)}
 			>
 				<div
 					className={`${focus && "text-amber-500"} 
