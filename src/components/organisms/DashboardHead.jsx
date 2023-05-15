0f2df75a4cfb6cb9f5c8fc8f28d9f88e1cbb96a2
Numonu <juan.villegas.dev@gmail.com>
2023-05-14 20:17:01 -0500

    he usado el hook useScrollView para animar el fondo del componente dashboardHead

--- src/components/organisms/DashboardHead.jsx
+++ src/components/organisms/DashboardHead.jsx
@@ -1,9 +1,15 @@
+import useScrollView from "@/hooks/useScrollView";
 import { HiBars3BottomLeft } from "react-icons/hi2";
 import { IoMdNotificationsOutline } from "react-icons/io";
 export default function DashboardHead() {
+	const {scrollOver} = useScrollView(1);
 	return (
 		<>
-			<header className="sticky top-0">
+			<header
+				className={`${
+					scrollOver && "bg-white shadow-sm"
+				} sticky top-0  transition-[colors_shadow]`}
+			>
 				<nav className="max-w-[1200px] mx-auto py-2 px-4 flex justify-between items-center text-3xl">
 					<HiBars3BottomLeft />
 					<h1 className="text-2xl">Neotask</h1>
