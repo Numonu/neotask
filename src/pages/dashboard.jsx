fe4060aac8a34f63dddc1aa32b1c2a7d48dece89
Numonu <juan.villegas.dev@gmail.com>
2023-05-14 20:17:36 -0500

    he importad el componente dashboardHead en la pagina dashboard

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,12 +1,15 @@
 import Head from "next/head";
+import DashboardHead from "@/components/organisms/DashboardHead";
 
-export default function Dashboard(){
-    return <>
-        <Head>
-            <title>Manage your Notes</title>
-        </Head>
-        <div>
-
-        </div>
-    </>
+export default function Dashboard() {
+	return (
+		<>
+			<Head>
+				<title>Manage your Notes</title>
+			</Head>
+			<div className="bg-neutral-50 text-neutral-800 h-[200vh]">
+				<DashboardHead/>
+			</div>
+		</>
+	);
 }
