ae03e58770442f82a988b6119423b16b7ec79859
Numonu <juan.villegas.dev@gmail.com>
2023-05-15 16:23:55 -0500

    he agregado un padding en el componente dashboardTasks

--- src/components/molecules/DashboardTasks.jsx
+++ src/components/molecules/DashboardTasks.jsx
@@ -3,8 +3,12 @@ import Task from "../atoms/Task";
 export default function DashboardTasks() {
 	return (
 		<>
-			<div className="grid grid-cols-1">
+			<div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
 				<Task
+					title={"Tengo cosas importantes este dia"}
+					date={"20 de marzo , 2023"}
+				/>
+                <Task
 					title={"Tengo cosas importantes este dia"}
 					date={"20 de marzo , 2023"}
 				/>
