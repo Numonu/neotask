2275a6069753931330c312f1edbf09e5fba6f9e8
Numonu <juan.villegas.dev@gmail.com>
2023-05-15 15:54:06 -0500

    he creado y maquetado el componente dashboardWorkspace

---
+++ src/components/organisms/DashboardWorkspace.jsx
@@ -0,0 +1,14 @@
+import DashboardFolders from "@/components/molecules/DashboardFolders";
+
+export default function DashboardWorkspace() {
+	return (
+		<>
+			<div className="max-w-[1200px] mx-auto px-4 grid grid-rows-2">
+				<DashboardFolders />
+                <div>
+                    
+                </div>
+			</div>
+		</>
+	);
+}
