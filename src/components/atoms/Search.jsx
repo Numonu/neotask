1612cb07bbfaf64c2a7f0f35e14fc56d843bee11
Numonu <juan.villegas.dev@gmail.com>
2023-05-14 20:46:08 -0500

    he maquetado el componente Search

--- src/components/atoms/Search.jsx
+++ src/components/atoms/Search.jsx
@@ -1,7 +1,19 @@
-export default function Search(){
-    return <>
-        <div>
-            
-        </div>
-    </>
+import { useState } from "react";
+import { BsSearch } from "react-icons/bs";
+export default function Search() {
+	const [focus, setFocus] = useState(false);
+
+	return (
+		<>
+			<div
+				className={`${focus ? "border-amber-500" : "border-black"} 
+                border-2 border-b-4 rounded-xl p-2 py-1 grid items-center grid-cols-[1fr_min-content] gap-2 transition-colors`}
+				onFocus={() => setFocus(true)}
+				onBlur={() => setFocus(false)}
+			>
+				<input className="text-xl" type="text" />
+				<BsSearch className={`${focus && "text-amber-500"} text-2xl`} />
+			</div>
+		</>
+	);
 }
