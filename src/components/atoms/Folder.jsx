33b2cfa74b1bfda51a60b18ab111c4e76de907e4
Numonu <juan.villegas.dev@gmail.com>
2023-05-15 17:18:31 -0500

    he reducido el tono de fondo del componente folder

--- src/components/atoms/Folder.jsx
+++ src/components/atoms/Folder.jsx
@@ -1,7 +1,7 @@
 export default function FolderButton({ icon, children }) {
 	return (
 		<>
-			<button className="bg-neutral-300 py-2 px-4 flex items-center gap-2 rounded-lg hover:bg-neutral-800 hover:text-neutral-50 hover:py-3 transition-[background-color_padding_color] focus:bg-neutral-800  focus:py-3 outline-amber-500 group">
+			<button className="bg-neutral-200 py-2 px-4 flex items-center gap-2 rounded-lg hover:bg-neutral-800 hover:text-neutral-50 hover:py-3 transition-[background-color_padding_color] focus:bg-neutral-800  focus:py-3 outline-amber-500 group">
 				<div className="text-2xl group-focus:text-amber-500">{icon}</div>
 				<span className="text-xl group-focus:text-amber-500">{children}</span>
 			</button>
