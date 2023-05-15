365d139fe573d97f0f75223631990ab52c72d712
Numonu <juan.villegas.dev@gmail.com>
2023-05-14 21:20:47 -0500

    he creado y maquetado el componente dashboardHero

---
+++ src/components/molecules/DashboardHero.jsx
@@ -0,0 +1,21 @@
+import Paragraph from "../atoms/Paragraph";
+import Search from "../atoms/Search";
+import Subtitle from "../atoms/Subtitle";
+import Title from "../atoms/Title";
+import { AiFillStar } from "react-icons/ai";
+
+export default function DashboardHero() {
+	return (
+		<>
+			<div className="max-w-[1200px] mx-auto px-4 py-5 flex flex-col gap-2">
+				<Subtitle className="-mb-4">welcome</Subtitle>
+				<Title>Numonu</Title>
+				<Paragraph className="mb-4">
+					Did you know that you can customize this welcome message in
+					the settings section ?
+				</Paragraph>
+				<Search />
+			</div>
+		</>
+	);
+}
