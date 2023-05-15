286ef7e690a6e1e083e6e63b44b345d342938457
Numonu <juan.villegas.dev@gmail.com>
2023-05-14 20:15:49 -0500

    he agregado la logica del hook useScrollView

--- src/hooks/useScrollView.jsx
+++ src/hooks/useScrollView.jsx
@@ -1,3 +1,24 @@
-export default function useScrollView(){
-    
+import { useState , useEffect } from "react";
+
+export default function useScrollView(threshold){
+    const [scrollOver, setScrollOver ] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const scrollValue = window.pageYOffset;
+			scrollValue > threshold && !scrollOver && setScrollOver(true);
+			scrollValue < threshold && scrollOver && setScrollOver(false);
+		};
+
+		window.addEventListener("scroll", handleScroll);
+
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, [scrollOver , threshold]);
+
+
+    return {
+        scrollOver
+    }
 }
