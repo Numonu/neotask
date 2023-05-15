d5c3e8aadcb7c4c043201c6aa44cdab02506be9f
Numonu <juan.villegas.dev@gmail.com>
2023-05-15 17:20:08 -0500

    he agregado un efecto de focus al componente task

--- src/components/atoms/Task.jsx
+++ src/components/atoms/Task.jsx
@@ -1,7 +1,7 @@
 export default function Task({title , date}) {
 	return (
 		<>
-			<button className="pattern-points border-neutral-800 border-2 border-b-4 px-4 py-6 rounded-md text-start">
+			<button className="pattern-points border-neutral-800 border-2 border-b-4 px-4 py-6 rounded-md text-start outline-offset-8 outline-amber-500">
 				<div className="border-neutral-800 w-9 aspect-square border-4 rounded-xl mb-8"></div>
 				<h2 className="font-bold text-3xl mb-2">
 					{title}
