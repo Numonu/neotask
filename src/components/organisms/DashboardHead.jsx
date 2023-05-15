52da4faa757531f54cd5ff72937fac8f3e2c24be
Numonu <juan.villegas.dev@gmail.com>
2023-05-15 13:17:31 -0500

    he importado el componente circleButton en el dashboardHead

--- src/components/organisms/DashboardHead.jsx
+++ src/components/organisms/DashboardHead.jsx
@@ -1,8 +1,9 @@
 import useScrollView from "@/hooks/useScrollView";
 import { HiBars3BottomLeft } from "react-icons/hi2";
 import { IoMdNotificationsOutline } from "react-icons/io";
+import CircleButton from "../atoms/CircleButton";
 export default function DashboardHead() {
-	const {scrollOver} = useScrollView(1);
+	const { scrollOver } = useScrollView(1);
 	return (
 		<>
 			<header
@@ -11,9 +12,13 @@ export default function DashboardHead() {
 				} sticky top-0  transition-[colors_shadow]`}
 			>
 				<nav className="max-w-[1200px] mx-auto py-2 px-4 flex justify-between items-center text-3xl">
-					<HiBars3BottomLeft />
+					<CircleButton>
+						<HiBars3BottomLeft />
+					</CircleButton>
 					<h1 className="text-2xl">Neotask</h1>
-					<IoMdNotificationsOutline />
+					<CircleButton>
+						<IoMdNotificationsOutline />
+					</CircleButton>
 				</nav>
 			</header>
 		</>
