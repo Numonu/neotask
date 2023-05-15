9965683eba990269d8e1a07f7de48b365ffa61e2
Numonu <juan.villegas.dev@gmail.com>
2023-05-14 19:04:04 -0500

    he creado la pagina dashboard

---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Dashboard(){
+    return <>
+        <Head>
+            <title>Manage your Notes</title>
+        </Head>
+        <div>
+
+        </div>
+    </>
+}
