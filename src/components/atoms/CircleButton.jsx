80b1f74295a9628a12992861841a78d4b35610c1
Numonu <juan.villegas.dev@gmail.com>
2023-05-16 13:57:46 -0500

    he aumentado el offset del outline en el componente circleButton

--- src/components/atoms/CircleButton.jsx
+++ src/components/atoms/CircleButton.jsx
@@ -1,7 +1,7 @@
 export default function CircleButton({children}) {
 	return (
 		<>
-			<button className="group relative p-2 cursor-pointer rounded-full outline-amber-500">
+			<button className="group relative p-2 cursor-pointer rounded-full outline-amber-500 outline-offset-8">
 				<div className="bg-neutral-200 absolute inset-0 rounded-full scale-0 group-hover:scale-100 group-focus:scale-100 transition-transform"></div>
 				<div className="relative">
                     {children}
