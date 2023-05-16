f024122d81eaeb0ed3e7176cc36cf1179d05f475
Numonu <juan.villegas.dev@gmail.com>
2023-05-16 12:30:21 -0500

    he actualizado la descripcion de la pagina signup

---
+++ src/app/signup/page.jsx
@@ -0,0 +1,18 @@
+import SignUpLayout from "../../components/organisms/SignUpLayout";
+
+const medatada = {
+    title: 'Sign up in Neotask',
+	description: 'Create an account and discover the wonderful benefits of a task manager, fast, easy and beautiful',
+}
+
+export default function Signup() {
+	return (
+		<>
+			<div className="text-neutral-800 bg-neutral-50">
+				<div className="min-h-screen max-w-xl mx-auto flex flex-col justify-center">
+					<SignUpLayout />
+				</div>
+			</div>
+		</>
+	);
+}
