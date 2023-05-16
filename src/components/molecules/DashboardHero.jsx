95190c03c9420eb1e5b42ef870e02de331d5c209
Numonu <juan.villegas.dev@gmail.com>
2023-05-16 17:34:02 -0500

    he eliminado la ilustracion del componente dashboarHero

--- src/components/molecules/DashboardHero.jsx
+++ src/components/molecules/DashboardHero.jsx
@@ -8,11 +8,6 @@ export default function DashboardHero() {
 	return (
 		<>
 			<div className="max-w-[1200px] mx-auto px-4 py-5 flex flex-col gap-2">
-				<MyImage 
-					src="svg/person.svg" 
-					width={60} 
-					height={60} 
-				/>
 				<Subtitle className="-mb-4">welcome</Subtitle>
 				<Title>Numonu</Title>
 				<Paragraph className="mb-4">
