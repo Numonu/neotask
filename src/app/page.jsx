512d2c97039c77e8bdb7c329bed7f7aaa9950918
Numonu <juan.villegas.dev@gmail.com>
2023-05-16 10:39:39 -0500

    he creado la carpeta app y he migrado la pagina home anterior a ella

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import HomeHero from "../components/organisms/HomeHero";
+export default function Home() {
+	return (
+		<>
+			<div className="pattern-radial-points text-neutral-800 min-h-screen">
+				<HomeHero />
+			</div>
+		</>
+	);
+}
