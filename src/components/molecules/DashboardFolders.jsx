fbdfb61eeacbdd4dfe971ec9a0f5da4a590209c2
Numonu <juan.villegas.dev@gmail.com>
2023-05-16 16:47:35 -0500

    he rediseñado el boton del componente dashboardFolders

--- src/components/molecules/DashboardFolders.jsx
+++ src/components/molecules/DashboardFolders.jsx
@@ -1,19 +1,20 @@
 import { AiOutlineFolderOpen } from "react-icons/ai";
+import { BsFillPlusCircleFill } from "react-icons/bs";
 import Folder from "../atoms/Folder";
+import SoulHover from "../atoms/SoulHover";
 
 export default function DashboardFolders() {
 	return (
 		<>
 			<div className="flex flex-col gap-1">
-				<Folder icon={<AiOutlineFolderOpen />}>
-                    All
-                </Folder>
-				<Folder icon={<AiOutlineFolderOpen />}>
-					General
-				</Folder>
-				<Folder icon={<AiOutlineFolderOpen />}>
-					Favorites
-				</Folder>
+				<Folder icon={<AiOutlineFolderOpen />}>All</Folder>
+				<Folder icon={<AiOutlineFolderOpen />}>General</Folder>
+				<Folder icon={<AiOutlineFolderOpen />}>Favorites</Folder>
+				<button className="text-5xl self-center mt-4 outline-amber-500 outline-offset-8 rounded-full">
+					<SoulHover>
+						<BsFillPlusCircleFill />
+					</SoulHover>
+				</button>
 			</div>
 		</>
 	);
