4be7311a0742af3009d78ef481d1959f71155940
Numonu <juan.villegas.dev@gmail.com>
2023-05-17 14:06:13 -0500

    he vuelto el signupForm un componente de cliente y creado el metodo de registro con email

--- src/components/molecules/SignUpForm.jsx
+++ src/components/molecules/SignUpForm.jsx
@@ -1,11 +1,31 @@
+"use client";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { AiOutlineUser } from "react-icons/ai";
 import { MdAlternateEmail } from "react-icons/md";
 import { BsShieldLock, BsGoogle } from "react-icons/bs";
 import { MdOutlineMail } from "react-icons/md";
 import Button from "../atoms/Button";
 import Input from "../atoms/Input";
+import { auth } from "@/libs/firebase.init";
+import { useState } from "react";
 
 export default function SignUpForm() {
+
+	const [credentials , setCredentials] = useState({
+		username : "",
+		email : "",
+		password : ""
+	})
+
+
+	const registerWithEmail = async () => {
+		try {
+			await createUserWithEmailAndPassword(auth , credentials.email , credentials.password);
+		} catch (error) {
+			console.error(error.code , error.message);
+		}
+	}
+
 	return (
 		<>
 			<form className="flex flex-col gap-4">
