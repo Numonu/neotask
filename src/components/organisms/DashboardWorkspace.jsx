4128f0753e38f6091843a90d0486fa5193e92cc0
Numonu <juan.villegas.dev@gmail.com>
2023-05-18 17:58:20 -0500

    he importado el componente search dentro del dashboardWorkspace

--- src/components/organisms/DashboardWorkspace.jsx
+++ src/components/organisms/DashboardWorkspace.jsx
@@ -1,11 +1,15 @@
 import DashboardFolders from "@/components/molecules/DashboardFolders";
+import Search from "../atoms/Search";
 import DashboardTasks from "../molecules/DashboardTasks";
 export default function DashboardWorkspace() {
 	return (
 		<>
-			<div className="max-w-[1200px] mx-auto px-4 grid grid-cols-1 gap-4 md:grid-cols-[1fr_3fr]">
-				<DashboardFolders />
-				<DashboardTasks/>
+			<div className="max-w-[1200px] mx-auto px-4">
+				<Search />
+				<div className="mt-4 grid grid-cols-1 gap-4 md:grid-cols-[1fr_3fr]">
+					<DashboardFolders />
+					<DashboardTasks />
+				</div>
 			</div>
 		</>
 	);
