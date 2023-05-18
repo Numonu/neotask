7c63e5d6d30a2534f0bc45d18dfccad8e9240108
Numonu <juan.villegas.dev@gmail.com>
2023-05-18 17:57:08 -0500

    he ajustado el padding en el componente dashboardHero

--- src/components/molecules/DashboardHero.jsx
+++ src/components/molecules/DashboardHero.jsx
@@ -9,11 +9,10 @@ export default function DashboardHero() {
 			<div className="max-w-[1200px] mx-auto px-4 py-5 flex flex-col gap-2">
 				<Subtitle className="-mb-4">welcome</Subtitle>
 				<Title>Numonu</Title>
-				<Paragraph className="mb-4">
+				<Paragraph>
 					Did you know that you can customize this welcome message in
 					the settings section ?
 				</Paragraph>
-				<Search />
 			</div>
 		</>
 	);
