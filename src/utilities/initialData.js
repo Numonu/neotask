7d473e6ada31844f6ae20dde3f99d3f4af1db63a
Numonu <juan.villegas.dev@gmail.com>
2023-05-18 18:03:33 -0500

    he creado la utilidad initialData

---
+++ src/utilities/initialData.js
@@ -0,0 +1,14 @@
+export const initialData = [
+	{
+		folderName: "all",
+		documents: [
+			{
+				title: "Initial Document",
+				content: "this es a example content!",
+			},
+		],
+		config: {
+			protecteed: true,
+		},
+	},
+];
