e20440f8dad1059c10887c9b2b264296d98797b2
Numonu <juan.villegas.dev@gmail.com>
2023-05-18 18:06:46 -0500

    he eliminado el reductor y el contexto de la pagina dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,39 +1,17 @@
-"use client";
 import DashboardHero from "@/components/molecules/DashboardHero";
 import DashboardWorkspace from "@/components/organisms/DashboardWorkspace";
-import { dataReducer } from "@/utilities/dataReducer";
-import { createContext, useReducer } from "react";
 
 export const metadata = {
     title : "Manage your Notes",
     description : "Create , update adn delete notes"
 }
 
-const initalData = [
-	{
-		folderName : "all",
-		documents : [	
-			{
-				title : "Initial Document",
-				content : "this es a example content!"
-			}
-		],
-		config : {
-			protecteed : true,
-		}
-	}
-]
-
-export const dataContext = createContext(null);
 export default function Dashboard() {
-	const [data , dispatch] = useReducer(dataReducer , initalData);
 	return (
 		<>
 			<div className="h-[200vh]">
-				<dataContext.Provider>
-                	<DashboardHero/>
-					<DashboardWorkspace/>
-				</dataContext.Provider>
+                <DashboardHero/>
+				<DashboardWorkspace/>
 			</div>
 		</>
 	);
