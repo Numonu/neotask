7a74b3ba79225a462f1c2ed148426d876046add8
Numonu <juan.villegas.dev@gmail.com>
2023-05-18 15:05:50 -0500

    he creado el reductor para los datos del dashboard

---
+++ src/utilities/dataReducer.js
@@ -0,0 +1,12 @@
+import { produce } from "immer";
+
+export const dataReducer = produce((draft , action) => {
+    switch (action.type) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+})
