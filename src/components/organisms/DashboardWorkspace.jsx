7300f149546b49871ea5e2f8ea694eae00ac4cc8
Numonu <juan.villegas.dev@gmail.com>
2023-05-18 18:13:58 -0500

    la logica del reductor ha hecho un componente de cliente el dashboardWorkspace

--- src/components/organisms/DashboardWorkspace.jsx
+++ src/components/organisms/DashboardWorkspace.jsx
@@ -1,7 +1,13 @@
+"use client";
 import DashboardFolders from "@/components/molecules/DashboardFolders";
+import { dataReducer } from "@/utilities/dataReducer";
+import { initialData } from "@/utilities/initialData";
+import { useReducer } from "react";
 import Search from "../atoms/Search";
 import DashboardTasks from "../molecules/DashboardTasks";
+
 export default function DashboardWorkspace() {
+	const [data, dispatch] = useReducer(dataReducer, initialData);
 	return (
 		<>
 			<div className="max-w-[1200px] mx-auto px-4">
