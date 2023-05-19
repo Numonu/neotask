719c98cf916d262b725b1a0d8fc04d7b17983b3a
Numonu <juan.villegas.dev@gmail.com>
2023-05-18 21:24:05 -0500

    he eliminado la utilidad generateId

--- src/utilities/generateId.js
+++
@@ -1,8 +0,0 @@
-export function generateId(seed){
-    const getRnd = (limit) => Math.floor(Math.random() * limit);
-
-    const [closureA , closureB] = [seed * getRnd(10) , seed * getRnd(10)];
-    const closureC = (closureA + closureB) * getRnd(10);
-
-    return closureC;
-}
