575e74ef1d9446c7eb0c1958c331db6c108c6ffd
Numonu <juan.villegas.dev@gmail.com>
2023-05-19 12:47:05 -0500

    he importado el contexto para rendizar las tareas de un folder especifico en el componente dashboardTasks

--- src/components/molecules/DashboardTasks.jsx
+++ src/components/molecules/DashboardTasks.jsx
@@ -1,17 +1,17 @@
+import { useContext } from "react";
 import Task from "../atoms/Task";
+import { dataContext } from "../organisms/DashboardWorkspace";
 
 export default function DashboardTasks() {
+	const {data , folderFocus} = useContext(dataContext);
 	return (
 		<>
 			<div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
-				<Task
-					title={"Tengo cosas importantes este dia"}
-					date={"20 de marzo , 2023"}
-				/>
-                <Task
-					title={"Tengo cosas importantes este dia"}
-					date={"20 de marzo , 2023"}
-				/>
+				{
+					data[folderFocus].documents.map(e => {
+						return <Task key={e.title} title={e.title} date={"20 marzo , 2023"}/>
+					})
+				}
 			</div>
 		</>
 	);
