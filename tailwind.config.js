e54b7b6dabc3b8986f323ad19cc7d8f16b217cd2
Numonu <juan.villegas.dev@gmail.com>
2023-05-19 14:30:38 -0500

    he creado un color para los outlines en la configuracion de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend : {
+      colors : {
+        outline : "#0062ff",
+      }
+    }
+  },
   plugins: [],
 }
