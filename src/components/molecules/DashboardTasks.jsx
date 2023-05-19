4011c47c7d820240c14ce9dc1fc341f54bf8235c
Numonu <juan.villegas.dev@gmail.com>
2023-05-19 13:50:03 -0500

    he importado el componente createDoc en dashboardTasks

--- src/components/molecules/DashboardTasks.jsx
+++ src/components/molecules/DashboardTasks.jsx
@@ -1,12 +1,14 @@
 import { useContext } from "react";
-import Task from "../atoms/Task";
 import { dataContext } from "../organisms/DashboardWorkspace";
+import CreateDoc from "../atoms/CreateDoc";
+import Task from "../atoms/Task";
 
 export default function DashboardTasks() {
 	const {data , folderFocus} = useContext(dataContext);
 	return (
 		<>
 			<div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+				<CreateDoc/>
 				{
 					data[folderFocus].documents.map(e => {
 						return <Task key={e.title} title={e.title} date={"20 marzo , 2023"}/>
