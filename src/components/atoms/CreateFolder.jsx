7b54080341fe91900c4c5e69939f9bd3398cd300
Numonu <juan.villegas.dev@gmail.com>
2023-05-19 16:13:20 -0500

    el componente CreateFolder llama un callback al hacerle click

--- src/components/atoms/CreateFolder.jsx
+++ src/components/atoms/CreateFolder.jsx
@@ -1,10 +1,13 @@
 import { BsFillPlusCircleFill } from "react-icons/bs";
 import SoulHover from "./SoulHover";
 
-export default function CreateFolder() {
+export default function CreateFolder({ callback }) {
 	return (
 		<>
-			<button className="text-5xl self-center mt-4 outline-amber-500 outline-offset-8 rounded-full">
+			<button
+				className="text-5xl self-center mt-4 outline-amber-500 outline-offset-8 rounded-full"
+				onClick={callback}
+			>
 				<SoulHover>
 					<BsFillPlusCircleFill />
 				</SoulHover>
