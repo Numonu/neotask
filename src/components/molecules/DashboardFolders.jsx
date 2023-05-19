95392473cbb5a67f1fe2b264060cf8694b2abb91
Numonu <juan.villegas.dev@gmail.com>
2023-05-19 14:36:29 -0500

    he importado el componente CreateFolder en dashboardFolders

--- src/components/molecules/DashboardFolders.jsx
+++ src/components/molecules/DashboardFolders.jsx
@@ -1,8 +1,7 @@
 import { useContext } from "react";
 import { AiOutlineFolderOpen } from "react-icons/ai";
-import { BsFillPlusCircleFill } from "react-icons/bs";
+import CreateFolder from "../atoms/CreateFolder";
 import Folder from "../atoms/Folder";
-import SoulHover from "../atoms/SoulHover";
 import { dataContext } from "../organisms/DashboardWorkspace";
 
 export default function DashboardFolders() {
@@ -19,11 +18,7 @@ export default function DashboardFolders() {
 						{e.folderName}
 					</Folder>
 				))}
-				<button className="text-5xl self-center mt-4 outline-amber-500 outline-offset-8 rounded-full">
-					<SoulHover>
-						<BsFillPlusCircleFill />
-					</SoulHover>
-				</button>
+				<CreateFolder/>
 			</div>
 		</>
 	);
