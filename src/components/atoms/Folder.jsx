7c08a72124208a110e06b4e20166970706845d51
Numonu <juan.villegas.dev@gmail.com>
2023-05-19 13:28:57 -0500

    ahora los components de folder usan el contexto para modificar el folderDocus

--- src/components/atoms/Folder.jsx
+++ src/components/atoms/Folder.jsx
@@ -1,9 +1,20 @@
-export default function FolderButton({ icon, children }) {
+import { useContext } from "react";
+import { dataContext } from "../organisms/DashboardWorkspace";
+
+export default function FolderButton({ icon, children, order }) {
+	const {setFolderFocus} = useContext(dataContext);
 	return (
 		<>
-			<button className="bg-neutral-200 py-2 px-4 flex items-center gap-2 rounded-lg hover:bg-neutral-800 hover:text-neutral-50 hover:py-3 transition-[background-color_padding_color] focus:bg-neutral-800  focus:py-3 outline-amber-500 group">
-				<div className="text-2xl group-focus:text-amber-500">{icon}</div>
-				<span className="text-xl group-focus:text-amber-500">{children}</span>
+			<button
+				className="bg-neutral-200 py-2 px-4 flex items-center gap-2 rounded-lg hover:bg-neutral-800 hover:text-neutral-50 hover:py-3 transition-[background-color_padding_color] focus:bg-neutral-800  focus:py-3 outline-amber-500 group"
+				onClick={() => setFolderFocus(order)}
+			>
+				<div className="text-2xl group-focus:text-amber-500">
+					{icon}
+				</div>
+				<span className="text-xl capitalize group-focus:text-amber-500">
+					{children}
+				</span>
 			</button>
 		</>
 	);
