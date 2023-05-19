cdc2b4f9e4baca26dd0769f883aef6101c000b3f
Numonu <juan.villegas.dev@gmail.com>
2023-05-19 12:41:13 -0500

    he creado el contexto para los datos y funciones en el dashboardWorkspace

--- src/components/organisms/DashboardWorkspace.jsx
+++ src/components/organisms/DashboardWorkspace.jsx
@@ -2,20 +2,26 @@
 import DashboardFolders from "@/components/molecules/DashboardFolders";
 import { dataReducer } from "@/utilities/dataReducer";
 import { initialData } from "@/utilities/initialData";
-import { useReducer } from "react";
+import { createContext, useReducer } from "react";
 import Search from "../atoms/Search";
 import DashboardTasks from "../molecules/DashboardTasks";
 
+export const dataContext = createContext(null);
 export default function DashboardWorkspace() {
 	const [data, dispatch] = useReducer(dataReducer, initialData);
 	return (
 		<>
 			<div className="max-w-[1200px] mx-auto px-4">
-				<Search />
-				<div className="mt-4 grid grid-cols-1 gap-4 md:grid-cols-[1fr_3fr]">
-					<DashboardFolders />
-					<DashboardTasks />
-				</div>
+				<dataContext.Provider value={{
+					data , 
+					dispatch
+				}}>
+					<Search />
+					<div className="mt-4 grid grid-cols-1 gap-4 md:grid-cols-[1fr_3fr]">
+						<DashboardFolders />
+						<DashboardTasks />
+					</div>
+				</dataContext.Provider>
 			</div>
 		</>
 	);
