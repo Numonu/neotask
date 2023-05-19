1ee04c2b1a2650e1ba8d0d99c123d2584211d169
Numonu <juan.villegas.dev@gmail.com>
2023-05-19 13:17:28 -0500

    ahora en la representacion de folders se usan el contexto , en el componente dashbaordFolders

--- src/components/molecules/DashboardFolders.jsx
+++ src/components/molecules/DashboardFolders.jsx
@@ -1,15 +1,20 @@
+import { useContext } from "react";
 import { AiOutlineFolderOpen } from "react-icons/ai";
 import { BsFillPlusCircleFill } from "react-icons/bs";
 import Folder from "../atoms/Folder";
 import SoulHover from "../atoms/SoulHover";
+import { dataContext } from "../organisms/DashboardWorkspace";
 
 export default function DashboardFolders() {
+	const { data } = useContext(dataContext);
 	return (
 		<>
 			<div className="flex flex-col gap-1">
-				<Folder icon={<AiOutlineFolderOpen />}>All</Folder>
-				<Folder icon={<AiOutlineFolderOpen />}>General</Folder>
-				<Folder icon={<AiOutlineFolderOpen />}>Favorites</Folder>
+				{data.map((e) => (
+					<Folder key={e.folderName} icon={<AiOutlineFolderOpen />}>
+						{e.folderName}
+					</Folder>
+				))}
 				<button className="text-5xl self-center mt-4 outline-amber-500 outline-offset-8 rounded-full">
 					<SoulHover>
 						<BsFillPlusCircleFill />
