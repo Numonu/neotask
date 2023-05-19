2ffc80cb8ede3c86b4b0f178ddbd72b7f7ae9230
Numonu <juan.villegas.dev@gmail.com>
2023-05-18 21:51:22 -0500

    he actualizado para usar la utilidad de formato en la pagina dashboard y sus rutas dinamicas

--- src/app/dashboard/[task_name]/page.jsx
+++ src/app/dashboard/[task_name]/page.jsx
@@ -2,10 +2,9 @@ import Title from "@/components/atoms/Title"
 import { unformatRoute } from "@/utilities/format"
 
 export default function TaskPage({params}){
-    const title = unformatRoute(params.task_name);
     return <>
         <div className="wrapper min-h-screen py-16">
-            <Title>{title}</Title>
+            <Title>{unformatRoute(params.task_name)}</Title>
         </div>
     </>
 }
