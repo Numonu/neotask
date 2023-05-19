f38ca472a93e37299da5929dfda77c9a4c2ddb29
Numonu <juan.villegas.dev@gmail.com>
2023-05-18 21:26:14 -0500

    he agregado la navegacion de los componentes task

--- src/components/atoms/Task.jsx
+++ src/components/atoms/Task.jsx
@@ -1,7 +1,15 @@
+import { useRouter } from "next/navigation";
+
 export default function Task({title , date}) {
+	const router = useRouter();
+	const formatedTitle = title.replace(/\s/g , "-");
+
+	const navToTask = () => router.push(`/dashboard/${formatedTitle}`);
+
 	return (
 		<>
-			<button className="pattern-points border-neutral-800 border-2 border-b-4 px-4 py-6 rounded-md text-start outline-offset-8 outline-amber-500 hover:-translate-y-2 group transition-transform">
+			<button className="pattern-points border-neutral-800 border-2 border-b-4 px-4 py-6 rounded-md text-start outline-offset-8 outline-amber-500 hover:-translate-y-2 group transition-transform"
+			onClick={navToTask}>
 				<div className="border-neutral-800 w-9 aspect-square border-4 rounded-xl mb-8 group-hover:rotate-45 transition-transform"></div>
 				<h2 className="font-bold text-3xl mb-2">
 					{title}
