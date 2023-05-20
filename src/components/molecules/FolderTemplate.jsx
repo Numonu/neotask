4d1334a1ac1591243d2bd520ff61610fac7c200f
Numonu <juan.villegas.dev@gmail.com>
2023-05-20 10:37:36 -0500

    el folderTemplate ahora puede crear nuevos folder usando el dispathc de los datos

--- src/components/molecules/FolderTemplate.jsx
+++ src/components/molecules/FolderTemplate.jsx
@@ -1,25 +1,40 @@
+import { useContext, useState } from "react";
 import { AiOutlineFolderOpen } from "react-icons/ai";
+import { dataContext } from "../organisms/DashboardWorkspace";
 
-export default function FolderTemplate({callback}) {
+export default function FolderTemplate({ callback }) {
+	const [folderName, setFolderName] = useState("");
+	const { dispatch } = useContext(dataContext);
 
-    const create = (e) => {
-        if(e == "Enter" || !e){
-            //here create a folder
-            callback();
-        }
-    }
+	const create = (e) => {
+		if (e == "Enter" || !e) {
+			callback();
+			dispatch({
+				type: "create-folder",
+				data: {
+					folderName,
+				},
+			});
+		}
+	};
 
 	return (
 		<>
 			<div
 				className="bg-neutral-800 text-neutral-50  py-3 px-4 flex items-center gap-2 rounded-lg hover:py-3 transition-[background-color_padding_color] outline-outline group"
-                onKeyDown={(e) => create(e.key)}
+				onKeyDown={(e) => create(e.key)}
 			>
 				<div className="text-2xl">
 					<AiOutlineFolderOpen />
 				</div>
 				<span className="text-xl capitalize">
-					<input type="text" className="bg-neutral-700" autoFocus="true" onBlur={() => create("Enter")}/>
+					<input
+						type="text"
+						className="bg-neutral-700"
+						autoFocus="true"
+						onBlur={() => create("Enter")}
+						onChange={(e) => setFolderName(e.target.value)}
+					/>
 				</span>
 			</div>
 		</>
