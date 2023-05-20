36f4dac27be6b7fbee223649be16e0bab5a694d8
Numonu <juan.villegas.dev@gmail.com>
2023-05-20 11:41:01 -0500

    he cambiado el nombre de las rutas dinamicas a 'doc_name'

--- src/app/dashboard/[doc_name]/page.jsx
+++ src/app/dashboard/[doc_name]/page.jsx
@@ -4,7 +4,7 @@ import { unformatRoute } from "@/utilities/format"
 export default function TaskPage({params}){
     return <>
         <div className="wrapper min-h-screen py-16">
-            <Title>{unformatRoute(params.task_name)}</Title>
+            <Title>{unformatRoute(params.doc_name)}</Title>
         </div>
     </>
 }
