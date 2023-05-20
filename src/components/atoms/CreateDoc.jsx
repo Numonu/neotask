5e1d47c057d0a3429d8c5b93a73765493dfad38d
Numonu <juan.villegas.dev@gmail.com>
2023-05-20 11:36:52 -0500

    el componente CreateDoc nos llevara a una ruta de documento vacia

--- src/components/atoms/CreateDoc.jsx
+++ src/components/atoms/CreateDoc.jsx
@@ -1,9 +1,13 @@
+import { useRouter } from "next/navigation";
 import { IoDocumentTextOutline } from "react-icons/io5";
 
 export default function CreateDoc() {
+	const router = useRouter();
+	const navToNew = () => router.push("dashboard/New-Document");
+
 	return (
 		<>
-			<button className="border-zinc-800 w-full h-full flex flex-col items-center justify-center gap-2 border-2 border-dashed cursor-pointer hover:bg-neutral-100 transition-colors outline-outline outline-offset-8">
+			<button className="border-zinc-800 w-full h-full min-h-[190px] flex flex-col items-center justify-center gap-2 border-2 border-dashed cursor-pointer hover:bg-neutral-100 outline-outline outline-offset-8 transition-colors" onClick={navToNew}>
 				<IoDocumentTextOutline className="text-3xl"/>
 				<h3 className="text-xl">Create New Document</h3>
 			</button>
