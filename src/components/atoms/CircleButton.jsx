112970ace9a6ae977159b3bd0d7391394e94f4ee
Numonu <juan.villegas.dev@gmail.com>
2023-05-21 18:25:09 -0500

    he actualizado el CircleButton para llamar un callback cuando se hace click en el

--- src/components/atoms/CircleButton.jsx
+++ src/components/atoms/CircleButton.jsx
@@ -1,11 +1,10 @@
-export default function CircleButton({children}) {
+export default function CircleButton({ children , onClick }) {
 	return (
 		<>
-			<button className="group relative p-2 cursor-pointer rounded-full outline-outline outline-offset-8">
+			<button className="w-min aspect-square group relative p-2 cursor-pointer rounded-full outline-outline outline-offset-8"
+			onClick={onClick}>
 				<div className="bg-neutral-200 absolute inset-0 rounded-full scale-0 group-hover:scale-100 group-focus:scale-100 transition-transform"></div>
-				<div className="relative">
-                    {children}
-                </div>
+				<div className="relative">{children}</div>
 			</button>
 		</>
 	);
