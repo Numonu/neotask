024ba7c5d6741cc789ee95c459ff928de9b6bb00
Numonu <juan.villegas.dev@gmail.com>
2023-05-21 13:46:39 -0500

    he eliminado el importe de virutalPopup en el dashboardTasks

--- src/components/molecules/DashboardTasks.jsx
+++ src/components/molecules/DashboardTasks.jsx
@@ -1,9 +1,8 @@
 "use client";
-import { dataContext } from "@/app/dashboard/providerLayout";
-import { useContext } from "react";
 import CreateDoc from "../atoms/CreateDoc";
 import Task from "../atoms/Task";
-import VirtualPopup from "../atoms/VirtualPopup";
+import { dataContext } from "@/app/dashboard/providerLayout";
+import { useContext } from "react";
 
 export default function DashboardTasks() {
 	const { data, folderFocus } = useContext(dataContext);
@@ -12,23 +11,7 @@ export default function DashboardTasks() {
 			<div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
 				<CreateDoc />
 				{data[folderFocus].documents.map((e, i) => {
-					return (
-						<VirtualPopup
-							key={e.title}
-							closeContent={
-								<Task title={e.title} date={e.date} />
-							}
-							openContent={
-								<Task title={e.title} date={e.date} />
-							}
-							openSize={{
-								width : "90vw",
-								height : "90vh",
-								maxWidth : "600px",
-								maxheight : "700px",
-							}}
-						/>
-					);
+					return <Task key={i} title={e.title} date={e.date} />;
 				})}
 			</div>
 		</>
