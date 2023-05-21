021943d7372a72917eb793419675d18de9899bf2
Numonu <juan.villegas.dev@gmail.com>
2023-05-21 17:28:03 -0500

    he agregado una animacion reveal al componente Modal

--- src/components/atoms/Modal.jsx
+++ src/components/atoms/Modal.jsx
@@ -7,7 +7,7 @@ export default function Modal({ children, callback }) {
 	return (
 		<div
 			ref={autoRef}
-			className="bg-neutral-900 bg-opacity-70 backdrop-blur-sm fixed z-[100] w-screen h-screen flex justify-center items-center"
+			className="bg-neutral-900 bg-opacity-70 animated-reveal backdrop-blur-sm fixed z-[100] w-screen h-screen flex justify-center items-center"
 			onClick={(e) => {
 				e.target == autoRef.current && callback();
 			}}
