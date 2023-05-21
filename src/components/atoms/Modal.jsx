a7e938ce23054c1bba7db7918c8390f372b9526b
Numonu <juan.villegas.dev@gmail.com>
2023-05-21 18:24:23 -0500

    he actualizado el modal para que llame un callback cuando se hace click sobre el

--- src/components/atoms/Modal.jsx
+++ src/components/atoms/Modal.jsx
@@ -9,7 +9,7 @@ export default function Modal({ children, callback }) {
 			ref={autoRef}
 			className="bg-neutral-900 bg-opacity-70 animated-reveal backdrop-blur-sm fixed z-[100] w-screen h-screen flex justify-center items-center"
 			onClick={(e) => {
-				e.target == autoRef.current && callback();
+				(e.target == autoRef.current ) && callback();
 			}}
 		>
 			{children}
