2e36a2eb8c284bf2341d4c8922fdb980ea841f20
Numonu <juan.villegas.dev@gmail.com>
2023-05-21 18:30:02 -0500

    he creado y maquetado el componente TaskModal

---
+++ src/components/molecules/TaskModal.jsx
@@ -0,0 +1,18 @@
+import { HiOutlineXMark } from "react-icons/hi2";
+import CircleButton from "../atoms/CircleButton";
+
+export default function TaskModal({ title , hanldeClose }) {
+	return (
+		<>
+			<div className="bg-neutral-50 h-[90vh] w-[90%] max-w-[600px] p-4 rounded-md grid grid-rows-[min-content_1fr] gap-4">
+				<div className="flex justify-between items-center">
+					<h3 className="text-xl text-center font-medium">{title}</h3>
+					<CircleButton onClick={hanldeClose}>
+						<HiOutlineXMark className="text-2xl" />
+					</CircleButton>
+				</div>
+				<textarea className="bg-neutral-100 text-lg p-2 rounded-md" />
+			</div>
+		</>
+	);
+}
