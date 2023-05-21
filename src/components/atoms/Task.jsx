d08c7b7f90b4a8ec78dd657ba95c371963b34cb6
Numonu <juan.villegas.dev@gmail.com>
2023-05-20 19:34:08 -0500

    he reducido el movimiento al hover en el componente Task

--- src/components/atoms/Task.jsx
+++ src/components/atoms/Task.jsx
@@ -7,7 +7,7 @@ export default function Task({order , title , date}) {
 
 	return (
 		<>
-			<button className="pattern-points border-neutral-800 border-2 border-b-4 px-4 py-6 rounded-md text-start outline-offset-8 outline-outline hover:-translate-y-2 group transition-transform"
+			<button className="pattern-points border-neutral-800 border-2 border-b-4 px-4 py-6 rounded-md text-start outline-offset-8 outline-outline hover:-translate-y-[0.2rem] hover:shadow-lg group transition-[transform_shadow]"
 			onClick={navToTask}>
 				<div className="border-neutral-800 w-9 aspect-square border-4 rounded-xl mb-8 group-hover:rotate-45 transition-transform"></div>
 				<h2 className="font-bold text-3xl mb-2">
