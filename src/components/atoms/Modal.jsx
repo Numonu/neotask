974480dc94c128875f33231145ddf0644116be55
Numonu <juan.villegas.dev@gmail.com>
2023-05-21 17:21:30 -0500

    he optimizado el uso del modal y he agregado un callback

--- src/components/atoms/Modal.jsx
+++ src/components/atoms/Modal.jsx
@@ -1,12 +1,18 @@
 "use client";
-import { createPortal } from "react-dom";
 
-export default function Modal({children}){
-    return <> 
-        {
-            createPortal( <div className="bg-neutral-900 bg-opacity-70 backdrop-blur-sm fixed z-50 w-screen h-screen flex justify-center items-center">
-                {children}
-            </div> , window.document.body)
-        }
-    </>
+import { useRef } from "react";
+
+export default function Modal({ children, callback }) {
+	const autoRef = useRef(null);
+	return (
+		<div
+			ref={autoRef}
+			className="bg-neutral-900 bg-opacity-70 backdrop-blur-sm fixed z-[100] w-screen h-screen flex justify-center items-center"
+			onClick={(e) => {
+				e.target == autoRef.current && callback();
+			}}
+		>
+			{children}
+		</div>
+	);
 }
