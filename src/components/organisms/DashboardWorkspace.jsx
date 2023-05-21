405b38987dba4524c929f9a9a57c80b9ebd4abc5
Numonu <juan.villegas.dev@gmail.com>
2023-05-20 20:10:06 -0500

    he eliminado el contexto local en el componente dashboardWorkspace

--- src/components/organisms/DashboardWorkspace.jsx
+++ src/components/organisms/DashboardWorkspace.jsx
@@ -1,34 +1,20 @@
-"use client";
 import DashboardFolders from "@/components/molecules/DashboardFolders";
-import { dataReducer } from "@/utilities/dataReducer";
-import { initialData } from "@/utilities/initialData";
-import { createContext, useReducer, useState } from "react";
 import Search from "../atoms/Search";
 import DashboardTasks from "../molecules/DashboardTasks";
 
-export const dataContext = createContext(null);
 export default function DashboardWorkspace() {
-	const [folderFocus , setFolderFocus] = useState(0);
-	const [data, dispatch] = useReducer(dataReducer, initialData);
 	return (
 		<>
 			<div className="max-w-[1200px] mx-auto px-4">
-				<dataContext.Provider value={{
-					data , 
-					dispatch,
-					folderFocus,
-					setFolderFocus,
-				}}>
-					<Search />
-					<div className="mt-4 flex flex-col gap-4 md:flex-row">
-						<div className="md:w-1/4">
-							<DashboardFolders />
-						</div>
-						<div className="md:w-3/4">
-							<DashboardTasks />
-						</div>
+				<Search />
+				<div className="mt-4 flex flex-col gap-4 md:flex-row">
+					<div className="md:w-1/4">
+						<DashboardFolders />
 					</div>
-				</dataContext.Provider>
+					<div className="md:w-3/4">
+						<DashboardTasks />
+					</div>
+				</div>
 			</div>
 		</>
 	);
