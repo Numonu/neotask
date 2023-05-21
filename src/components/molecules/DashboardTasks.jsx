c82f695da535e161de71c3f92dd07bf85ecc00d5
Numonu <juan.villegas.dev@gmail.com>
2023-05-21 12:40:12 -0500

    he envuelto todas las tareas en un virtualpopup en el componente dsahboardTasks

--- src/components/molecules/DashboardTasks.jsx
+++ src/components/molecules/DashboardTasks.jsx
@@ -3,18 +3,33 @@ import { dataContext } from "@/app/dashboard/providerLayout";
 import { useContext } from "react";
 import CreateDoc from "../atoms/CreateDoc";
 import Task from "../atoms/Task";
+import VirtualPopup from "../atoms/VirtualPopup";
 
 export default function DashboardTasks() {
-	const {data , folderFocus} = useContext(dataContext);
+	const { data, folderFocus } = useContext(dataContext);
 	return (
 		<>
 			<div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
-				<CreateDoc/>
-				{
-					data[folderFocus].documents.map((e,i) => {
-						return <Task order={i} key={e.title} title={e.title} date={e.date}/>
-					})
-				}
+				<CreateDoc />
+				{data[folderFocus].documents.map((e, i) => {
+					return (
+						<VirtualPopup
+							key={e.title}
+							closeContent={
+								<Task title={e.title} date={e.date} />
+							}
+							openContent={
+								<Task title={e.title} date={e.date} />
+							}
+							openSize={{
+								width : "90vw",
+								height : "90vh",
+								maxWidth : "600px",
+								maxheight : "700px",
+							}}
+						/>
+					);
+				})}
 			</div>
 		</>
 	);
