cf6dc857b469a65d77e486f06092be71956ba785
Numonu <juan.villegas.dev@gmail.com>
2023-05-22 16:49:13 -0500

    el componente createDoc usa el reductor para crear nuevas tareas

--- src/components/atoms/CreateDoc.jsx
+++ src/components/atoms/CreateDoc.jsx
@@ -1,16 +1,22 @@
 import { dataContext } from "@/app/dashboard/providerLayout";
-import { useRouter } from "next/navigation";
 import { useContext } from "react";
 import { IoDocumentTextOutline } from "react-icons/io5";
 
 export default function CreateDoc() {
-	const router = useRouter();
-	const {data , folderFocus} = useContext(dataContext);
-	const navToNew = () => router.push(`dashboard/${data[folderFocus].documents.length}`);
+	const {folderFocus , dispatch} = useContext(dataContext);
+	
+	const create = () => {
+		dispatch({
+			type : "create-document",
+			data : {
+				folderOrder : folderFocus
+			}
+		})
+	}
 
 	return (
 		<>
-			<button className="border-zinc-800 w-full h-full min-h-[190px] flex flex-col items-center justify-center gap-2 border-2 border-dashed cursor-pointer hover:bg-neutral-100 outline-outline outline-offset-8 transition-colors" onClick={navToNew}>
+			<button className="border-zinc-800 w-full h-full min-h-[190px] flex flex-col items-center justify-center gap-2 border-2 border-dashed cursor-pointer hover:bg-neutral-100 outline-outline outline-offset-8 transition-colors" onClick={create}>
 				<IoDocumentTextOutline className="text-3xl"/>
 				<h3 className="text-xl">Create New Document</h3>
 			</button>
