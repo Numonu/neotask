72e286fef91120040d24dc91a3b281d81f0015d9
Numonu <juan.villegas.dev@gmail.com>
2023-05-22 17:49:28 -0500

    el titulo del TaskModal modifica el titulo de su documento correspondiente suando el reductor

--- src/components/molecules/TaskModal.jsx
+++ src/components/molecules/TaskModal.jsx
@@ -1,14 +1,14 @@
 import { dataContext } from "@/app/dashboard/providerLayout";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { HiOutlineXMark } from "react-icons/hi2";
 import CircleButton from "../atoms/CircleButton";
 
 export default function TaskModal({ data, order, hanldeClose }) {
 	const { folderFocus, dispatch } = useContext(dataContext);
 
-	const updateContent = (e) => {
+	const updateContent = (type , e) => {
 		dispatch({
-			type: "update-document",
+			type: type,
 			data: {
 				folderOrder: folderFocus,
 				docOrder: order,
@@ -26,6 +26,7 @@ export default function TaskModal({ data, order, hanldeClose }) {
 						value={data.title}
 						className="bg-neutral-100 focus:outline-outline p-2 rounded-md grow text-xl font-medium outline-offset-8 focus:outline-8"
 						placeholder="Title content"
+						onChange={(e) => updateContent("rename-document" , e.target.value)}
 					/>
 					<CircleButton onClick={hanldeClose}>
 						<HiOutlineXMark className="text-2xl" />
@@ -35,7 +36,7 @@ export default function TaskModal({ data, order, hanldeClose }) {
 					placeholder="Content Here"
 					value={data.content}
 					className="bg-neutral-100 focus:outline-outline text-lg p-2 rounded-md outline-offset-8 focus:outline-8"
-					onChange={(e) => updateContent(e.target.value)}
+					onChange={(e) => updateContent("update-document" , e.target.value)}
 				/>
 			</div>
 		</>
