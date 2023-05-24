470764121a2798f85cf67b78f3f871994254bb25
Numonu <juan.villegas.dev@gmail.com>
2023-05-24 15:56:38 -0500

    el hook useLogin carga los datos del usuario que inicia sesion

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -1,5 +1,6 @@
-import { auth, googleAuth } from "@/firebase/config";
+import { auth, db, googleAuth } from "@/firebase/config";
 import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 
@@ -7,21 +8,24 @@ export function useLogin(method , credentials) {
 	const router = useRouter();
 	return async () => {
 		const loader = toast.loading("Sending");
+		let userRequest;
 		try {
 			switch (method) {
 				case "email":
-					await signInWithEmailAndPassword(
+					userRequest = await signInWithEmailAndPassword(
 						auth,
 						credentials.email,
 						credentials.password
 					);
 					break;
 				case "google":
-					await signInWithPopup(auth, googleAuth);
+					userRequest = await signInWithPopup(auth, googleAuth);
 					break;
                 default :
                     throw new Error("[Dev]: el metodo " + method + " no esta declarado");
 			}
+			//antes de continuar , descargamos los datos de este usuario
+			await loopTryLoadData(userRequest.user.uid);
 			toast.dismiss(loader);
 			router.push("/dashboard");
 		} catch (error) {
@@ -29,4 +33,17 @@ export function useLogin(method , credentials) {
 			toast.error(error.message, { id: loader });
 		}
 	};
+}
+const loopTryLoadData = async (uid) => {
+	while (true) {
+		try {
+			const dataRequest = await getDoc(doc(db, "users", uid));
+			localStorage["neotask-local-data"] = dataRequest.data().data;
+			break;
+		} catch (error) {
+			await new Promise((res) => setTimeout(res , 5000))
+			console.error(error);
+			continue;
+		}
+	}
 }
