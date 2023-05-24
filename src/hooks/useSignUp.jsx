1b83a2ad0e642cca4d143d9fa25b960aedb9cc06
Numonu <juan.villegas.dev@gmail.com>
2023-05-24 14:10:00 -0500

    el hook useSignUp ahora crea tambien un espacio en la base de datos para nuestros usuarios registrados

--- src/hooks/useSignUp.jsx
+++ src/hooks/useSignUp.jsx
@@ -1,32 +1,59 @@
-import { auth, googleAuth } from "@/firebase/config";
+import { auth, db, googleAuth } from "@/firebase/config";
 import { createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 
-export function useSignUp(method , credentials) {
+export function useSignUp(method, credentials) {
 	const router = useRouter();
 	return async () => {
 		const loader = toast.loading("Sending");
+		//Peticion de usuario local
+		let userRequest;
+		//Registramos el usuario usando sus credenciales
 		try {
 			switch (method) {
 				case "email":
-					await createUserWithEmailAndPassword(
+					userRequest = await createUserWithEmailAndPassword(
 						auth,
 						credentials.email,
 						credentials.password
 					);
 					break;
 				case "google":
-					await signInWithPopup(auth, googleAuth);
+					userRequest = await signInWithPopup(auth, googleAuth);
 					break;
-                default :
-                    throw new Error("[Dev]: el metodo " + method + " no esta declarado");
+				default:
+					throw new Error(
+						"[Dev]: el metodo " + method + " no esta declarado"
+					);
 			}
+			//finalmente redirigimos a la ruta dashboard
 			toast.dismiss(loader);
 			router.push("/dashboard");
+			//Creamos un hueco en la base de datos para este usuario (5 Intentos max)
+			loopTryCreateUser(userRequest.user.uid , credentials.username);
 		} catch (error) {
 			console.error(error.code, error.message);
 			toast.error(error.message, { id: loader });
 		}
 	};
 }
+
+const loopTryCreateUser = async (uid , username) => {
+	alert(username);
+	let intentos = 1;
+	while (intentos < 5) {
+		intentos++;
+		try {
+			await setDoc(doc(db, "users", uid), {
+				username : username,
+				data : "",
+			});
+			break;
+		} catch (e) {
+			await new Promise((res) => setTimeout(res , 5000))
+			continue;
+		}
+	}
+};
