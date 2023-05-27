7dcc44b6335be9f44a1c71ba9fc321575a1dbf7d
Numonu <juan.villegas.dev@gmail.com>
2023-05-26 22:48:43 -0500

    el componente providerLayout guarda el estado en la base de datos

--- src/app/dashboard/providerLayout.jsx
+++ src/app/dashboard/providerLayout.jsx
@@ -1,15 +1,37 @@
 "use client";
+import { db } from "@/firebase/config";
 import { dataReducer } from "@/utilities/dataReducer";
 import { initialData } from "@/utilities/initialData";
-import { createContext, useEffect, useReducer, useState } from "react";
+import { doc, updateDoc } from "firebase/firestore";
+import { createContext, useContext, useEffect, useReducer, useState } from "react";
+import { authContext } from "./authLayout";
 
 export const dataContext = createContext(null);
 export default function ProviderLayout({ children }) {
+	const user = useContext(authContext);
+	
+	const [saveData , setSaveData] = useState(true);
 	const [folderFocus, setFolderFocus] = useState(0);
-	const [data, dispatch] = useReducer(dataReducer, JSON.parse(localStorage.getItem("neotask-local-data")) || initialData);
+	
+	const loadThis = localStorage["neotask-local-data"] ? JSON.parse(localStorage["neotask-local-data"]) : null;
+	const [data, dispatch] = useReducer(dataReducer ,loadThis || initialData);
+	console.log("data is " , data);
+	console.log("load is " , loadThis);
 
 	useEffect(() => {
 		localStorage.setItem("neotask-local-data" , JSON.stringify(data));
+		if(saveData && user){
+			updateDoc(doc(db , "users" , user.uid) , {
+				data : JSON.stringify(data)
+			}).then(() => {
+				setTimeout(() => {
+					setSaveData(true);
+				} , 15000)
+			}).catch(e => {
+				console.error(e);
+			})
+			setSaveData(false);
+		}
 	} , [data])
 
 	return (
